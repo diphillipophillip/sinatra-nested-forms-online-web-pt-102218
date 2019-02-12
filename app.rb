cc21482943349d0724c49ede2e11cb835227e33c
Phillip DiPhillipo <diphillipophillip@gmail.com>
2019-02-12 03:13:24 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,8 +14,10 @@ module FormsLab
     post '/pirates' do 
       @pirate = Pirate.new(params[:pirate])
       
+      
       params[:pirate][:ships].each do |details| 
         Ship.new(details)
+      
     end 
     @ships = Ship.all 
     erb :'pirates/show'
